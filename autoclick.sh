3c5893f3828bbcab8ea9f41edb0c527c6c77d372
rotr1926-boop <rotr1926-boop@users.noreply.github.com>
2026-06-25 16:12:35 +0200

    Translate autoclick.sh comments to English

--- autoclick.sh
+++ autoclick.sh
@@ -9,21 +9,21 @@ if [ -f "$PIDFILE" ]; then
 fi
 
 (
-# 🔹 CLICK INIZIALE (UNA SOLA VOLTA)
+# 🔹 INITIAL CLICK (ONCE)
 xdotool mousemove 1580 438
 sleep 1
 xdotool click 1
 sleep 1
 
-# 🔁 LOOP PRINCIPALE
+# 🔁 MAIN LOOP
 while true; do
-    # Punto 1
+    # Point 1
     xdotool mousemove 148 1204
     sleep 0.1
     xdotool click 1
     sleep 0.15
 
-    # Punto 2
+    # Point 2
     xdotool mousemove 284 1091
     sleep 0.1
     xdotool click 1
